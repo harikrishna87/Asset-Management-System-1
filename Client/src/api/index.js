0adb129f82c91320884cb9a39bc91acc3f0f2758
harikrishna87 <krishnahari5048@gmail.com>
2025-06-01 19:28:34 +0530

    Asset Management System

--- Client/src/api/index.js
+++ Client/src/api/index.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_BASE_URL = 'https://asset-management-task.onrender.com';
+const API_BASE_URL = 'https://asset-management-system-1-csb0.onrender.com';
 
 const apiClient = axios.create({
   baseURL: API_BASE_URL,
